fbca983afabe9600316a583d7b061976666f80fe
LuthfiaHasan <luthfiahasana853@gmail.com>
2025-04-24 19:54:57 +0700

    refactor

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,34 +4,50 @@
 
 @section('content')
 
+<!-- Notifikasi sukses -->
 @if (session('success'))
     <div class="alert alert-success">
         {{ session('success') }}
     </div>    
 @endif
 
+<!-- Judul section -->
 <h1>Popular Movie</h1>
+
+<!-- Daftar Movie -->
 <div class="row">
     @foreach ($movies as $movie)
     <div class="col-lg-6">
-        <div class="card mb-3" style="max-width: 540px;">
+        <div class="card mb-3 shadow-sm" style="max-width: 540px;">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="/images/{{ $movie['foto_sampul'] }}" class="img-fluid rounded-start" alt="...">
-            </div>
+                <!-- Gambar sampul -->
+                <div class="col-md-4">
+                    <img src="/images/{{ $movie['foto_sampul'] }}" 
+                         class="img-fluid rounded-start" 
+                         alt="{{ $movie['judul'] }}">
+                </div>
+                
+                <!-- Detail movie -->
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">{{ $movie['judul'] }}</h5>
-                        <p class="card-text">{{ $movie['sinopsis'] }}</p>
-                        <a href="/movie/{{ $movie['id'] }}" class="btn btn-success">Lihat Selanjutnya</a>
+                        <p class="card-text text-truncate" style="max-height: 4.5em; overflow: hidden;">
+                            {{ $movie['sinopsis'] }}
+                        </p>
+                        <a href="/movie/{{ $movie['id'] }}" class="btn btn-success">
+                            Lihat Selanjutnya
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     @endforeach
-    <div class="d-flex justify-content-center">
-        {{ $movies->links() }}
-    </div>
 </div>
+
+<!-- Pagination -->
+<div class="d-flex justify-content-center mt-3">
+    {{ $movies->links() }}
+</div>
+
 @endsection
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -1,46 +1,64 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Metadata dasar -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>tiMovie - @yield('title', 'Website')</title>
+
+    <!-- Bootstrap CSS -->
     <link href="/bootstrap/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
+
+    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-success" data-bs-theme="dark">
-        <div class="container">
-          <a class="navbar-brand" href="/">tiMovie</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Watchlist</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/movies/create">Input Movie</a>
-              </li>
-            </ul>
-            <form action="/" class="d-flex" role="search">
-              <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-light" type="submit">Search</button>
-            </form>
-          </div>
+      <div class="container">
+        <!-- Brand / Logo -->
+        <a class="navbar-brand" href="/">tiMovie</a>
+
+        <!-- Toggle button for mobile view -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Collapsible navbar content -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left navigation links -->
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Watchlist</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/movies/create">Input Movie</a>
+            </li>
+          </ul>
+
+          <!-- Search form -->
+          <form action="/" class="d-flex" role="search">
+            <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light" type="submit">Search</button>
+          </form>
         </div>
-      </nav>
-
-      <div class="container my-2">
-        @yield('content')
       </div>
+    </nav>
 
-      <footer class="bg-success text-center text-white py-2">
-        Copyright &copy; 2023 by Yori Adi Atma
-      </footer>
+    <!-- Main Content Area -->
+    <div class="container my-4">
+      @yield('content')
+    </div>
 
+    <!-- Footer -->
+    <footer class="bg-success text-center text-white py-3">
+      &copy; 2023 by Yori Adi Atma
+    </footer>
+
+    <!-- Bootstrap JS Bundle -->
     <script src="/bootstrap/bootstrap.bundle.min.js"></script>
   </body>
 </html>
